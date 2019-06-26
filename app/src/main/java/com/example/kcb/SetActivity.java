5e43b57be0fef4946b3efc259ace7edc833a3749
Divisadero_o <1148639271@qq.com>
2019-06-27 05:00:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kcb/SetActivity.java
@@ -0,0 +1,20 @@
+package com.example.kcb;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class SetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+
+}
+}
